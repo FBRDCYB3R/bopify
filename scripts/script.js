f2aeea42ca0892c1ae7a4a3900f887847cad9b14
FBRDCYB3R <85688976+FBRDCYB3R@users.noreply.github.com>
2022-10-20 12:57:46 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ new Vue({
           name: "Fly Me To The Moon Lofi Cover",
           artist: "Yung Prod",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
-          source: "https://github.com/FBRDCYB3R/bopify/blob/master/mp3/Fly%20Me%20To%20The%20Moon%20Lofi%20Cover%20Prod%20YungRhythm.mp3",
+          source: "https://raw.githubusercontent.com/FBRDCYB3R/bopify/master/mp3/Fly Me To The Moon Lofi Cover Prod YungRhythm.mp3",
           url: "https://www.youtube.com/watch?v=g7lcmk6rxhE",
           favorited: false
         },
